d6f14c6abdc3535c86b18e14270c425da9961db7
Agoziemax <agoziemax@yahoo.com>
2023-05-18 23:42:30 +0100

    new updates

--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,33 +1,29 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState('');
 
-function BookCreate({onCreate}) {
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
-const[title, setTitle] = useState('')
-
-const handleChange = (e) => {
-
-setTitle(e.target.value)
-
-}
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  onCreate(title);
-  setTitle('')
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreate(title);
+    setTitle('');
+  };
 
   return (
     <div className='book-create'>
       <h3>Add a book</h3>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title</label>
-        <input className='input'id='title' onChange={handleChange} value={title} placeholder='Enter book title'/>
-        <button className='button'>Create!</button>
+        <label htmlFor='title'>Title</label>
+        <input className='input' id='title' onChange={handleChange} value={title} placeholder='Enter book title' />
+        <button className='button'>Create Book!</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default BookCreate
+export default BookCreate;
