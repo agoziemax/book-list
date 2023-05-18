f4952a42eab0db562b4292678c960535e2dc1e1d
Agoziemax <agoziemax@yahoo.com>
2023-05-18 07:33:32 +0100

    first commit

---
+++ src/components/BookCreate.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+function BookCreate({onCreate}) {
+
+const[title, setTitle] = useState('')
+
+const handleChange = (e) => {
+
+setTitle(e.target.value)
+
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  onCreate(title);
+  setTitle('')
+}
+
+  return (
+    <div className='book-create'>
+      <h3>Add a book</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input className='input'id='title' onChange={handleChange} value={title} placeholder='Enter book title'/>
+        <button className='button'>Create!</button>
+      </form>
+    </div>
+  )
+}
+
+export default BookCreate
